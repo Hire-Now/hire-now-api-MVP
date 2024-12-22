48d0c9716f64a2ba37bc2d5870c41bc7062913a2
Nicodav28 <esteveznicolas0@gmail.com>
2024-12-22 11:56:17 -0500

    ♻️ (CandidateController.php): Refactor CandidateController to improve readability and consistency The changes were made to improve the readability and consistency of the code. The constructor was refactored to have a body, which makes it easier to add more dependencies in the future. The variable name 'entity' was changed to 'candidate' to better reflect its purpose. The key 'Candidate' in the response was also changed to 'candidate' to follow the convention of using lowercase for keys.

--- app/Infrastructure/Controllers/CandidateController.php
+++ app/Infrastructure/Controllers/CandidateController.php
@@ -3,21 +3,23 @@
 namespace App\Infrastructure\Controllers;
 
 use App\Application\UseCases\CandidateUseCase;
-use App\Application\DTOs\CandidateDTO;
+use App\Application\DTOs\Candidate\CandidateDTO;
 use Illuminate\Http\Request;
 
 class CandidateController
 {
-    public function __construct(private CandidateUseCase $useCase) {}
+    public function __construct(private CandidateUseCase $useCase)
+    {
+    }
 
     public function store(Request $request)
     {
         $dto = CandidateDTO::fromRequest($request->all());
-        $entity = $this->useCase->execute($dto);
+        $candidate = $this->useCase->execute($dto);
 
         return response()->json([
-            'message' => 'Candidate created successfully!',
-            'Candidate' => $entity
+            'message'   => 'Candidate created successfully!',
+            'candidate' => $candidate
         ]);
     }
 }
