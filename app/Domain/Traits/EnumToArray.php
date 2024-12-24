062215f6f7e81b67d509908d2340fc1f67c8a755
Nicodav28 <esteveznicolas0@gmail.com>
2024-12-24 15:16:16 -0500

    ♻️ (EnumToArray.php): Replace 'self' keyword with 'static' for late static binding The 'self' keyword was replaced with 'static' in the EnumToArray trait to allow for late static binding. This change provides more flexibility, as it allows the trait to be used in classes that may have different implementations of the 'cases', 'values', and 'names' methods.

--- app/Domain/Traits/EnumToArray.php
+++ app/Domain/Traits/EnumToArray.php
@@ -6,16 +6,16 @@ trait EnumToArray
 {
     public static function names(): array
     {
-        return array_column(self::cases(), 'name');
+        return array_column(static::cases(), 'name');
     }
 
     public static function values(): array
     {
-        return array_column(self::cases(), 'value');
+        return array_column(static::cases(), 'value');
     }
 
     public static function array(): array
     {
-        return array_combine(self::values(), self::names());
+        return array_combine(static::values(), static::names());
     }
 }
